9e10f12db77ac62c6c3c518d03d852ec116451d9
Abhirams2020 <abhirams2020@gmail.com>
2021-09-19 11:56:48 +0530

    fixed connection matrix

--- ACO/aco.py
+++ ACO/aco.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from presets import n_ants, iterations
 
 class ant_colony:
@@ -72,31 +73,36 @@ class ant_colony:
                 denominator += (self.pheromone[start_node_index][n_index]**self.alpha
                                 *self.visibility[start_node_index][n_index]**self.beta)
             probability = ((self.pheromone[start_node_index][next_node_index]**self.alpha
-                            *self.visibility[start_node_index][next_node_index]**self.beta)*self.connection[start_node_index][next_node_index]/denominator)
+                            *self.visibility[start_node_index][next_node_index]**self.beta)/denominator)
             probabilities.append(probability)
         self.probabilities = probabilities
 
-    # Function to calculates probability and chooses the appropriate route. Also updates the pheromones.
+    # Function to calculates probability and chooses the appropriate route. Also updates the pheromones
     def optimise(self):
         self.get_adjacency()
         self.initialise_pheromone()
         self.initial_route = list(self.nodes)
-        self.route_best = self.initial_route
-        self.distance_best = self.get_total_distance(self.route_best) 
+        # self.route_best = self.initial_route
+        self.route_best = []
+        self.distance_best = math.inf
         for i in range(iterations):
             local_pheromone = np.zeros((self.adjacency.shape[0],self.adjacency.shape[0]))
             for b in range(n_ants):
                 self.initialise_route()
                 route = list(self.start_node)
                 current_node_index = self.start_node_index
+                templist = self.unvisited_nodes
                 while len(self.unvisited_nodes) != 0:
-                    self.get_probabilities(current_node_index,self.unvisited_nodes)
-                    max_prob = max(self.probabilities)
-                    max_prob_index = self.probabilities.index(max_prob)
-                    next_node = self.unvisited_nodes[max_prob_index]
-                    route.append(next_node)
-                    current_node_index = self.initial_route.index(next_node)
-                    self.unvisited_nodes.remove(next_node)
+                    self.get_probabilities(current_node_index,templist)
+                    next_node = np.random.choice(templist,p=self.probabilities)
+                    next_node_index = self.initial_route.index(next_node)
+                    if self.connection[current_node_index][next_node_index]==0:
+                        templist.remove(next_node)
+                    else:
+                        route.append(next_node)
+                        current_node_index = self.initial_route.index(next_node)
+                        self.unvisited_nodes.remove(next_node)
+                self.route_best
                 distance = self.get_total_distance(route)
                 new_pheromone = np.zeros((self.adjacency.shape[0],self.adjacency.shape[0]))
                 for node in range(len(route)):
@@ -108,11 +114,9 @@ class ant_colony:
                         next_node = route[0]
                     j = self.initial_route.index(next_node) 
                     new_pheromone[i][j] = self.p/distance
-            
-                    
+                if (distance < self.distance_best) and (len(route)==len(self.nodes)):
+                    self.route_best, self.distance_best = route, distance
                 local_pheromone = local_pheromone + new_pheromone
-            
-                self.route_best, self.distance_best = route, distance
             print(route, np.round(distance,4))
             self.pheromone = ((1-self.evaporation)*self.pheromone + local_pheromone)
         
