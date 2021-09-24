52f3ed465d2dc35914485254a7a480a8972855a9
Abhirams2020 <abhirams2020@gmail.com>
2021-09-24 07:27:43 +0530

    updated images folder

--- ACO/presets.py
+++ ACO/presets.py
@@ -5,8 +5,8 @@
 # beta : Constant to control the effect of local preference matrix H.
 # n_ants : Number of ants used per iteration.
 # iterations : Number of iterations.
+nodes = {'A':(4,2), 'B':(5,1), 'C':(6,3), 'D':(2,5), 'E':(1,1)}
 
-nodes = {'A':(1,4), 'B':(1,1), 'C':(2,6), 'D':(3,1), 'E':(4,2)}
 p = 100
 evaporation = 0.5
 alpha = 1
@@ -15,6 +15,9 @@ n_ants = 100
 iterations = 20
 connection = [  [0,0,1,1,1],
                 [0,0,1,1,1],
-                [1,1,0,1,0],
-                [1,1,1,0,1],
-                [1,1,0,1,0]]
+                [1,1,0,1,1],
+                [1,1,1,0,0],
+                [1,1,1,0,0]]
+
+
+
